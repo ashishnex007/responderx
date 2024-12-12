36a7950c3b0da485660a44aac11a26b6ac199738
AshishGowtham <122021623+AshishGowtham@users.noreply.github.com>
2024-12-12 14:05:19 +0530

    changed structure

--- index.js
+++ index.js
@@ -19,95 +19,95 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 // Set the frontend folder as the static folder
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, './frontend')));
 
 // Serve index.html on the / endpoint
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/landing.html'));
+  res.sendFile(path.join(__dirname, './frontend/landing.html'));
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/login.html'));
+  res.sendFile(path.join(__dirname, './frontend/login.html'));
 });
 
 app.get('/signup', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/register.html'));
+  res.sendFile(path.join(__dirname, './frontend/register.html'));
 });
 
 app.get('/dashboard', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dashboard.html'));
+  res.sendFile(path.join(__dirname, './frontend/dashboard.html'));
 });
 
 app.get('/volunteer_dashboard', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/volunteer_dashboard.html'));
+  res.sendFile(path.join(__dirname, './frontend/volunteer_dashboard.html'));
 });
 
 app.get('/submit_report', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/incident_reporting.html'));
+  res.sendFile(path.join(__dirname, './frontend/incident_reporting.html'));
 });
 
 app.get('/reports', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/view_reports.html'));
+  res.sendFile(path.join(__dirname, './frontend/view_reports.html'));
 });
 
 app.get('/view_report', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/report_detail.html'));
+  res.sendFile(path.join(__dirname, './frontend/report_detail.html'));
 });
 
 app.get('/admin', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/admin.html'));
+  res.sendFile(path.join(__dirname, './frontend/admin.html'));
 });
 
 app.get('/government', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/government.html'));
+  res.sendFile(path.join(__dirname, './frontend/government.html'));
 });
 
 app.get('/ration_management', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/ration.html'));
+  res.sendFile(path.join(__dirname, './frontend/ration.html'));
 });
 
 app.get('/register_volunteer', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/volunteers.html'));
+  res.sendFile(path.join(__dirname, './frontend/volunteers.html'));
 });
 
 app.get('/all_volunteers', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/all_volunteers.html'));
+  res.sendFile(path.join(__dirname, './frontend/all_volunteers.html'));
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/contact.html'));
+  res.sendFile(path.join(__dirname, './frontend/contact.html'));
 });
 
 app.get('/chat', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/chatbot.html'));
+  res.sendFile(path.join(__dirname, './frontend/chatbot.html'));
 });
 
 app.get('/resources', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/resources.html'));
+  res.sendFile(path.join(__dirname, './frontend/resources.html'));
 });
 
 app.get('/donate', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/donate.html'));
+  res.sendFile(path.join(__dirname, './frontend/donate.html'));
 });
 
 app.get('/donate/money', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/donate_money.html'));
+  res.sendFile(path.join(__dirname, './frontend/donate_money.html'));
 });
 
 app.get('/donate/resources', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/donate_resources.html'));
+  res.sendFile(path.join(__dirname, './frontend/donate_resources.html'));
 });
 
 app.get('/view_donations', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/view_donations.html'));
+  res.sendFile(path.join(__dirname, './frontend/view_donations.html'));
 });
 
 app.get('/view_resources', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/view_resources.html'));
+  res.sendFile(path.join(__dirname, './frontend/view_resources.html'));
 });
 
 app.get('/logout', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/logout.html'));
+  res.sendFile(path.join(__dirname, './frontend/logout.html'));
 });
 
 // Backend routes
